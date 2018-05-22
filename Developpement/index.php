5f709a4f1b3db9996506a847f69721e66e63e376
antoine acquart <antoine.acquart@ynov.com>
2018-05-22 11:58:26 +0200

    test

--- Developpement/index.php
+++ Developpement/index.php
@@ -1,50 +1,9 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Page d'accueil</title>
-    <link rel="stylesheet" href="inc/css/bootstrap.css">
-    <link rel="stylesheet" href="inc/css/abeezecss.css">
-  </head>
-  <body>
-    <header>
-      <div class="container">
-        <div class="row align-items-center">
-          <div class="col-7">
-            <img class="logo" src="inc/img/logo.png" alt="Logo anysol">
-          </div>
-          <div class="col-2">
-          </div>
-          <div class="col-2">
-            <form class="cherche" method="post">
-              <input type="text" name="recherche" value="Rechercher">
-              <div class="break">
-              </div>
-              <div class="btn-group" role="group" aria-label="Basic example">
-                <button type="button" class="btn btn-outline-primary btn-sm">Inscription</button>
+<?php
+include ("inc/header.inc.php");
+?>
 
-                <button type="button" class="btn btn-outline-primary btn-sm">Connexion</button>
-              </div>
-            </div>
-            </form>
 
-          </div>
-        </div>
-        <div class="row">
-            <div class="col-md-10 offset-md-1">
-              <nav class="nav nav-fill">
-              <a class="nav-item nav-link bg-info text-white" href="#">Accueil</a>
-              <a class="nav-item nav-link bg-info text-white" href="#">Nouveaut&eacute;</a>
-              <a class="nav-item nav-link bg-info text-white" href="#">Cat&eacute;gories</a>
-              <a class="nav-item nav-link bg-info text-white" href="#">Albums</a>
-              <a class="nav-item nav-link bg-info text-white" href="#">Auteurs</a>
-              </nav>
-            </div>
-        </div>
-        </div>
-    </header><br><br>
-
-    <div class="container">
+<div class="container">
       <div class="row cor">
         <div class="col-md-2 offset-md-1">
           <img class="placeholdere" src="inc/img/album6.jpg" alt="">
@@ -97,5 +56,7 @@
       </div>
     </div>
 
-  </body>
-</html>
+<?php
+include ("inc/footer.inc.php");
+?>
+
