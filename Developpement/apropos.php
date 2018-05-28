f3f16a1e5fb62f897908654affa770d0903a161c
unknown <stacy.perales@ynov.com>
2018-05-28 17:10:51 +0200

    apropos

---
+++ Developpement/apropos.php
@@ -0,0 +1,18 @@
+<?php
+include("inc/header.inc.php");
+ ?>
+ <div class="propos">
+   <h1>A propos du site</h1>
+         <p>Anysol est un site de partage de musique imaginé par 5 étudiants: Olivier Philibert, Lorenzo Logos, Stacy Perales, Nicolas Gerard et Antoine Acquart en 2018.
+           Ce projet a pris place pour un cours de développement Web au sein du campus Ynov Aix.</p>
+         <h2>Comment fonctionne Anysol?</h2>
+         <p>La particularité d'Anysol réside dans son système de playlist.
+           En effet, après avoir ajouter vos musiques dans votre playlist, vous pouvez, en cliquant sur l'onglet playlist, directement avoir accès à celle ci.
+           L'avantage ? Vos musiques préférées en un clic, dans votre site.<p>
+         <h2>A votre écoute</h2>
+         <p>Anysol possède aussi divers moyens de communication et nous n'hésiterons pas à vous répondre le plus rapidement possible.</p>
+
+
+ <?php
+include("inc/footer.inc.php");
+  ?>
--- Developpement/inc/footer.inc.php
+++ Developpement/inc/footer.inc.php
@@ -4,7 +4,7 @@ echo "<footer class=\"footer\">
 <div class=\"droite\">
   <a href=\"./contact.php\">Nous contactez</a><br>
     <a href=\"./condition.php\"> Condition Générales</a><br>
-  <a href=\"#\">A propos</a><br>
+  <a href=\"./apropos.php\">A propos</a><br>
 
 </div>
 <div class=\"gauche\">
