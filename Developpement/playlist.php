19c9ab7c09f94e8ced17c0a25ea0c49138b0f5d0
antoine acquart <antoine.acquart@ynov.com>
2018-05-29 00:36:27 +0200

    dernières modifs antoine

--- Developpement/playlist.php
+++ Developpement/playlist.php
@@ -7,13 +7,14 @@ if(isset($_SESSION['UserID'])){
 
     $requete_playlist = executeRequete("SELECT * FROM playlist WHERE UserID = ".$_SESSION['UserID']);
     $liste_playlist = $requete_playlist -> fetch_assoc();
-
+    echo'<div class="conteneurEcoute">';
     foreach ($requete_playlist as $liste_playlist){
-        echo '<div class="ecoute"><br><a href="pagecoute.php?playlistID='.$liste_playlist['PlaylistID'].'">
+        echo '<div class="divEcoute"><br><a href="pagecoute.php?playlistID='.$liste_playlist['PlaylistID'].'">
         Nom : '.$liste_playlist['PlaylistNom'].'<br><br>
         Description : '.$liste_playlist['PlaylistDesc'].'
         </a><br></div>';
     }
+    echo'</div>';
 }
 else{
     echo '<div class="ecoute">Vous devez être connecté pour pouvoir accéder à vos playlist</div>';
