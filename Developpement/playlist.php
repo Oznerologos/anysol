e4db62075749651064145fdb4d89d16f0076afb5
antoine acquart <antoine.acquart@ynov.com>
2018-05-29 12:44:01 +0200

    suppression playlist

--- Developpement/playlist.php
+++ Developpement/playlist.php
@@ -12,9 +12,29 @@ if(isset($_SESSION['UserID'])){
         echo '<div class="divEcoute"><br><a href="pagecoute.php?playlistID='.$liste_playlist['PlaylistID'].'">
         Nom : '.$liste_playlist['PlaylistNom'].'<br><br>
         Description : '.$liste_playlist['PlaylistDesc'].'
-        </a><br></div>';
+        </a><br>
+        <form method="POST" action="'.$_SERVER["PHP_SELF"].'"><br><br>
+            <input type="hidden" name="PlaylistID" value="'.$liste_playlist['PlaylistID'].'"/>
+            <input type="submit" name="supprimer" value="Supprimer la playlist"/>
+        </form><br></div>';
     }
     echo'</div>';
+
+    if(isset($_POST['supprimer'])){
+
+        $playlistID = $_POST['PlaylistID'];
+
+        $requete_verification_hidden = executeRequete("SELECT UserID FROM Playlist WHERE PlaylistID = ". $playlistID);
+        $liste_verification_hidden = $requete_verification_hidden -> fetch_assoc();
+
+        if ($liste_verification_hidden['UserID'] == $_SESSION['UserID']) { // on vérifie que l'utilisateur n'ait pas changé le champ hidden
+
+            $requete_PlaylistID = executeRequete("DELETE FROM link_musique_playlist WHERE PlaylistID = " . $playlistID);
+            $requete_PlaylistID = executeRequete("DELETE FROM playlist WHERE PlaylistID = " . $playlistID);
+        }
+
+        header('Location: '.$_SERVER["PHP_SELF"]);
+    }
 }
 else{
     echo '<div class="ecoute">Vous devez être connecté pour pouvoir accéder à vos playlist</div>';
@@ -22,5 +42,6 @@ else{
 echo '</div>';
 
 echo '<fieldset class="pub"><legend>Publicité</legend></fieldset>';
+
 include("inc/footer.inc.php");
 ?>
